3b0e6d0a2119ed5b2e6afa55f9cbe8837295eaf8
Maksim Zubkov <maksim.v.zubkov@gmail.com>
2020-11-29 18:54:12 +0300

    separator

--- export.rb
+++
@@ -1,6 +0,0 @@
-require "jekyll-import";
-JekyllImport::Importers::WordpressDotCom.run({
-  "source" => "/home/penguin/tmp/russianpenguin.wordpress.com-2020-11-29-13_23_37/wordpress.2020-11-29.001.xml",
-  "no_fetch_images" => false,
-  "assets_folder" => "assets/images"
-})
